364d9d50a4dd5fc1938a39ae29ac93c662bdbd66
Subrata34 <subratamodak3491@gmail.com>
2022-06-19 14:09:35 +0600

    files

---
+++ user-define-types/union-types.js
@@ -0,0 +1,8 @@
+var userID;
+userID = "101";
+userID = 101;
+function displayunion(userID) {
+    console.log(userID);
+}
+displayunion(100);
+displayunion("500");
---
+++ user-define-types/union-types.ts
@@ -0,0 +1,9 @@
+let userID:string|number;
+userID="101";
+userID=101;
+
+function displayunion(userID:string|number){
+    console.log(userID);
+}
+displayunion(100);
+displayunion("500");
