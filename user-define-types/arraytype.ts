6b72fe7797bc6b89f2b1e30b0ad4e30ec0c0d5e9
Subrata34 <subratamodak3491@gmail.com>
2022-06-23 11:59:34 +0600

    files

--- user-define-types/arraytype.ts
+++ user-define-types/arraytype.ts
@@ -4,3 +4,4 @@ console.log(username[0]);
 let multipletype:(string|number)[];
 multipletype=[23,'subrata','anisul'];
 console.log(multipletype[0]);
+
---
+++ user-define-types/tupletype.ts
@@ -0,0 +1,5 @@
+//mixed data --key,value
+let user:[number,string];
+
+user=[101,'Anis'];
+console.log(user);
