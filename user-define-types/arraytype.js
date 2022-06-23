4f3c7dca343b45fdc9a37cd2c072f6a489de87e3
Subrata34 <subratamodak3491@gmail.com>
2022-06-23 11:26:29 +0600

    files

--- user-define-types/arraytype.js
+++ user-define-types/arraytype.js
@@ -1,3 +1,6 @@
 var username;
 username = ['anis', 'aminul', 'subrata'];
 console.log(username[0]);
+var multipletype;
+multipletype = [23, 'subrata', 'anisul'];
+console.log(multipletype[0]);
--- user-define-types/arraytype.ts
+++ user-define-types/arraytype.ts
@@ -1,3 +1,6 @@
 let username:string[];
 username=['anis','aminul','subrata']
 console.log(username[0]);
+let multipletype:(string|number)[];
+multipletype=[23,'subrata','anisul'];
+console.log(multipletype[0]);
